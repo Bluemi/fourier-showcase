c1d486da80887fba5e300392cea362bcc4597622
Bluemi <bruno.schilling@protonmail.ch>
2023-06-17 12:27:24 +0200

    implemented it

--- src/main.py
+++ src/main.py
@@ -1,8 +1,133 @@
-#!/usr/bin/env python3
+import pygame as pg
+import numpy as np
+import scipy.fft
+
+
+IMAGE_SIZE = 100
+IMAGE_SHAPE = (IMAGE_SIZE,) * 2
+
+RENDER_SIZE = 600
+BORDER = 10
+IMAGE_RECT = pg.Rect(BORDER, BORDER, RENDER_SIZE, RENDER_SIZE)
+FREQUENCY_IMAGE_RECT1 = IMAGE_RECT.move(RENDER_SIZE + BORDER, 0)
+FREQUENCY_IMAGE_RECT2 = FREQUENCY_IMAGE_RECT1.move(RENDER_SIZE + BORDER, 0)
+
+DEFAULT_SCREEN_SIZE = np.array([BORDER + (RENDER_SIZE + BORDER)*3 + BORDER, RENDER_SIZE+BORDER*2])
+
+
+def gray(b):
+    return pg.Color(b, b, b)
+
+
+class Main:
+    def __init__(self):
+        self.running = True
+        self.screen = pg.display.set_mode(DEFAULT_SCREEN_SIZE)
+        self.space_image = np.zeros(IMAGE_SHAPE)
+        self.frequency_space = np.zeros(IMAGE_SHAPE, dtype=complex)
+        self.update_needed = True
+
+        self.drawing = False
+        self.last_flipped_index = None
+
+    def run(self):
+        while self.running:
+            events = [pg.event.wait()]
+            for event in events + pg.event.get():
+                self.handle_event(event)
+            if self.update_needed:
+                self.render()
+                self.update_needed = False
+
+    def render(self):
+        self.screen.fill(gray(127))
+
+        print(np.max(self.space_image))
+        image = image_from_np2d(self.space_image.real, RENDER_SIZE)
+        self.screen.blit(image, IMAGE_RECT)
+
+        frequency_image1 = image_from_np2d(self.frequency_space.real, RENDER_SIZE)
+        self.screen.blit(frequency_image1, FREQUENCY_IMAGE_RECT1)
+
+        frequency_image2 = image_from_np2d(self.frequency_space.imag, RENDER_SIZE)
+        self.screen.blit(frequency_image2, FREQUENCY_IMAGE_RECT2)
+
+        pg.display.update()
+
+    def handle_event(self, event):
+        if event.type == pg.QUIT:
+            self.running = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # print(self.frequency_space.flags)
+            # print(np.imag(self.frequency_space).copy().flags)
+            self.flip_point(event.pos)
+            self.drawing = True
+        if event.type == pg.MOUSEBUTTONUP:
+            self.drawing = False
+            self.last_flipped_index = None
+        if event.type == pg.MOUSEMOTION:
+            if self.drawing:
+                self.flip_point(event.pos)
+        else:
+            # print(event)
+            pass
+
+    def flip_point(self, pos):
+        rects = [IMAGE_RECT, FREQUENCY_IMAGE_RECT1, FREQUENCY_IMAGE_RECT2]
+        rect_index = -1
+        rect = None
+        for i, r in enumerate(rects):
+            if r.collidepoint(pos):
+                rect_index = i
+                rect = r
+                break
+        rect_top_left = np.array(rect.topleft)
+        mouse_pos = np.array(pos)
+        index = ((mouse_pos - rect_top_left) * (IMAGE_SIZE / RENDER_SIZE)).astype(int)
+        index = tuple(np.minimum(np.maximum(index, 0), IMAGE_SIZE - 1))
+
+        flipped_index = (*index, rect_index)
+        if self.last_flipped_index == flipped_index:
+            return
+        self.last_flipped_index = flipped_index
+
+        # mutate image
+        if rect_index == 0:
+            self.space_image[index] = 1 - self.space_image[index]
+        elif rect_index == 1:
+            self.frequency_space.real[index] = 1 - self.frequency_space.real[index]
+        elif rect_index == 2:
+            imag_cpy = np.imag(self.frequency_space).copy()
+            imag_cpy[index] = 1 - imag_cpy[index]
+            self.frequency_space.imag = imag_cpy
+        if rect_index == 0:
+            self.frequency_space = scipy.fft.fft2(self.space_image)
+        elif rect_index in (1, 2):
+            self.space_image = scipy.fft.ifft2(self.frequency_space)
+        self.update_needed = True
+
+
+def image_from_np2d(a, scale_shape):
+    if isinstance(scale_shape, int):
+        scale_shape = (scale_shape,) * 2
+    a = a.real
+    mini = np.min(a)
+    if mini < 0:
+        a = a - np.min(a)
+    maxi = np.max(a)
+    if maxi > 1:
+        a /= np.max(a)
+    a = a * 255
+    shape = a.shape
+    a = np.repeat(a, 3, axis=-1).reshape(*shape, 3)
+    image = pg.surfarray.make_surface(a.astype(int))
+    return pg.transform.scale(image, scale_shape)
 
 
 def main():
-    print('hello world!')
+    pg.init()
+    main_instance = Main()
+    main_instance.run()
 
 
 if __name__ == '__main__':
